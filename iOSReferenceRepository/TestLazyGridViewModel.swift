06b5a7847fb737e96572e367fa5504066cd4216e
HSaito <hisanoridevelop@gmail.com>
2023-04-26 09:39:02 +0900

    #10 GridViewを実装

---
+++ iOSReferenceRepository/TestLazyGridViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  TestLazyGridViewModel.swift
+//  iOSReferenceRepository
+//  
+//  Created by hisanori on 2023/04/21.
+//  
+
+
+import Foundation
+import SwiftUI
+
+class TestLazyGridViewModel: ObservableObject {
+    struct GridData: Hashable {
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(price)
+            hasher.combine(isSoldOut)
+        }
+        let thumbImage: Image
+        let price: Int
+        let isSoldOut: Bool
+    }
+
+   @Published var gridData: [GridData] = []
+
+    func fetchData() {
+        gridData = createData()
+    }
+
+    private func createData() -> [GridData] {
+        var data: [GridData] = []
+        for i in 1 ... 10 {
+            data.append(GridData(thumbImage: Image(uiImage: R.image.sarunori()!),
+                                 price: i * 1000,
+                                 isSoldOut: i % 2 == 0))
+        }
+        return data
+    }
+}
