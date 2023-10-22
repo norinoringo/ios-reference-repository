86b18d34ba02bf1e45b279eabe174962feda9c2e
HSaito <hisanoridevelop@gmail.com>
2023-10-23 08:52:09 +0900

    #43 HomeViewModelへのバインディングをNotificationで仮実装

--- iOSReferenceRepository/View/Home/HomeViewController.swift
+++ iOSReferenceRepository/View/Home/HomeViewController.swift
@@ -5,13 +5,39 @@
 //  Created by hisanori on 2023/10/19.
 //  
 
-
 import Foundation
 import UIKit
 
 class HomeViewController: UITabBarController {
+
+    private let viewModel = HomeViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Home画面表示")
+        bind()
+
+        homeViewDidLoad()
+    }
+
+    private func bind() {
+        addObserveNotifications()
+    }
+
+    private func addObserveNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleHomeViewDidLoad),
+            name: Notification.Name.Home.homeViewDidLoad,
+            object: nil
+        )
+    }
+
+    @objc func handleHomeViewDidLoad(_ notification: Notification) {
+        print("handleHomeViewDidLoad")
+    }
+
+    private func homeViewDidLoad() {
+        viewModel.homeViewDidLoad()
     }
 }
