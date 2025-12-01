3f60a1784ab801e49a8832a40b88b58cf61e1d78
norinoringo <hi.saito.se@gmail.com>
2025-12-01 17:47:05 +0900

    Handling user input Section 3 Done

--- iOSReferenceRepository/SwiftUITutorials/LandmarkList.swift
+++ iOSReferenceRepository/SwiftUITutorials/LandmarkList.swift
@@ -9,14 +9,29 @@
 import SwiftUI
 
 struct LandmarkList: View {
+
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
     var body: some View {
         if #available(iOS 16.0, *) {
             NavigationSplitView {
-                List(landmarks, id: \.id) { landmark in
-                    NavigationLink {
-                        LandmarkDetail(landmark: landmark)
-                    } label: {
-                        LandmarkRow(landmark: landmark)
+                List {
+                    Toggle(isOn: $showFavoritesOnly) {
+                        Text("Favorites only")
+                    }
+
+                    ForEach (filteredLandmarks) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
                     }
                 }
                 .navigationTitle("Landmark")
